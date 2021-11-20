ac6d17fda854a91dc9a7105a5b084a2a046a8311
oldnavy281 <oldnavy281@gmail.com>
2021-11-19 17:46:21 -0700

    tf removal

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 exports.index = (req, res) => {
     res.render('index', {
-        title: 'This is the backend. How tf did you get here?',
+        title: 'This is the backend. How did you get here?',
         config
     });
 };
