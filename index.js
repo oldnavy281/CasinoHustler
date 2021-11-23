1dfa413a23ad5f4895ed52e91ea2009732f125c9
oldnavy281 <oldnavy281@gmail.com>
2021-11-23 09:50:48 -0700

    more backend wheee

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.post('/', urlEncodedParser, (req, res) => {
+    console.log(req.body.username);
+    if(req.body.username == 'user' && req.body.password == 'pass') {
+        req.session.user = {
+            isAuthenticated: true,
+            username: req.body.username
+        };
+        res.redirect('/private');
+    } else {
+        res.redirect('/');
+    }
+});
